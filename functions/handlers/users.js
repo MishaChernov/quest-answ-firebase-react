930582fe2625c28aaf00e3f57d26e0c6409b2ca6
Mykhailo Chernov <m.chernov@astoundcommerce.com>
2020-09-27 13:00:09 +0300

    ADD: /login endpoint validation

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -6,18 +6,29 @@ const { uuid } = require('uuidv4');
 const firebase = require('firebase');
 firebase.initializeApp(config);
 
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.match(regEx);
+};
+
+const isEmpty = (string) => string.trim() === '';
+
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
         password: req.body.password,
     };
+
     let errors = {};
-    if (req.body.email.trim() === '') {
-        errors.email = 'Must not be empty ';
+
+    if (isEmpty(req.body.email)) {
+        errors.email = 'Must not be empty';
+    } else if (!isEmail(req.body.email)) {
+        errors.email = 'Must be a valid email address';
     }
 
-    if (req.body.password.trim() === '') {
-        errors.password = 'Must not be empty ';
+    if (isEmpty(req.body.password)) {
+        errors.password = 'Must not be empty';
     }
 
     if (Object.keys(errors).length > 0) res.status(400).json(errors);
